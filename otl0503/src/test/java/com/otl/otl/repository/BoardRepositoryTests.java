89f866652c1a9ddd750a00a019ac430b7e5db945
jidefine <youjio2000@gmail.com>
2024-05-07 01:20:52 +0900

    게시판/페이징 처리 중 오류 발견

--- otl0503/src/test/java/com/otl/otl/repository/BoardRepositoryTests.java
+++ otl0503/src/test/java/com/otl/otl/repository/BoardRepositoryTests.java
@@ -4,9 +4,16 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+
 import com.otl.otl.domain.Board;
 import com.otl.otl.domain.Member;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -50,13 +57,12 @@ public class BoardRepositoryTests {
 
     @Test
     public void testSelect() {
-        int board_id = 10;
+        int board_id = 9;
 
-        Long bno = Long.valueOf(board_id); // int를 Long으로 변환
-
-        Optional<Board> result = boardRepository.findById(bno);
+        Optional<Board> result = boardRepository.findById((long) board_id);
 
         Board board = result.orElseThrow();
+
 
         log.info(board);
 
@@ -67,9 +73,7 @@ public class BoardRepositoryTests {
 
         int board_id = 9;
 
-        Long bno = Long.valueOf(board_id); // int를 Long으로 변환
-
-        Optional<Board> result = boardRepository.findById(bno);
+        Optional<Board> result = boardRepository.findById((long) board_id);
 
         Board board = result.orElseThrow();
 
@@ -83,9 +87,27 @@ public class BoardRepositoryTests {
     public void testDelete() {
         int board_id = 10;
 
-        Long bno = Long.valueOf(board_id); // int를 Long으로 변환
-
-        boardRepository.deleteById(bno);
+        boardRepository.deleteById((long) board_id);
     }
 
+    @Test
+    public void testPaging() {
+
+        //1 page order by bno desc
+        Pageable pageable = PageRequest.of(0,10, Sort.by("board_id").descending());
+
+        Page<Board> result = boardRepository.findAll(pageable);
+
+
+        log.info("total count: "+result.getTotalElements());
+        log.info("total pages:" +result.getTotalPages());
+        log.info("page number: "+result.getNumber());
+        log.info("page size: "+result.getSize());
+
+        List<Board> boardList = result.getContent();
+
+        boardList.forEach(board -> log.info(board));
+
+
+    }
 }
