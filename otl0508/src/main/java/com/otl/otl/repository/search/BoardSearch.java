8f381c313d14cf2c4c6affc7f10d364e29cc6ac8
jidefine <youjio2000@gmail.com>
2024-05-09 04:51:29 +0900

    게시판/목록, 검색 기능 구현 재재시도

--- otl0508/src/main/java/com/otl/otl/repository/search/BoardSearch.java
+++ otl0508/src/main/java/com/otl/otl/repository/search/BoardSearch.java
@@ -7,8 +7,12 @@ import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 public interface BoardSearch {
-    List<Board> findByBoardTitleContaining(String keyword); // 제목에 특정 키워드가 포함된 게시물 검색
+//    List<Board> findByBoardTitleContaining(String keyword); // 제목에 특정 키워드가 포함된 게시물 검색
+//
+//    // 삭제되지 않은 게시물을 페이지로 가져옵니다.
+//    Page<Board> findByIsDeletedFalse(Pageable pageable);
 
-    // 삭제되지 않은 게시물을 페이지로 가져옵니다.
-    Page<Board> findByIsDeletedFalse(Pageable pageable);
+    Page<Board> search1(Pageable pageable);
+
+    Page<Board> searchAll(String[] types, String keyword, Pageable pageable);
 }
--- otl0508/src/test/java/com/otl/otl/repository/BoardRepositoryTests.java
+++ otl0508/src/test/java/com/otl/otl/repository/BoardRepositoryTests.java
@@ -1,2 +1,72 @@
-package com.otl.otl.repository;public class BoardRepositoryTests {
+package com.otl.otl.repository;
+
+//import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import com.otl.otl.domain.Board;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+@SpringBootTest
+//@Log4j2
+public class BoardRepositoryTests {
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @Test
+    public void testSearch1() {
+
+        //2 page order by bno desc
+        Pageable pageable = PageRequest.of(1,10, Sort.by("bno").descending());
+
+        boardRepository.search1(pageable);
+
+    }
+
+    @Test
+    public void testSearchAll() {
+
+        String[] types = {"t","c"}; // ,"w"는 닉네임으로 받을 예정
+
+        String keyword = "1";
+
+        Pageable pageable = PageRequest.of(0,10, Sort.by("bno").descending());
+
+        Page<Board> result = boardRepository.searchAll(types, keyword, pageable );
+
+    }
+
+    @Test
+    public void testSearchAll2() {
+
+        String[] types = {"t","c"}; // ,"w"는 닉네임으로 받을 예정
+
+        String keyword = "1";
+
+        Pageable pageable = PageRequest.of(0,10, Sort.by("bno").descending());
+
+        Page<Board> result = boardRepository.searchAll(types, keyword, pageable );
+
+        //total pages
+        //log.info(result.getTotalPages());
+
+        //pag size
+        //log.info(result.getSize());
+
+        //pageNumber
+        //log.info(result.getNumber());
+
+        //prev next
+        //log.info(result.hasPrevious() +": " + result.hasNext());
+
+        //result.getContent().forEach(board -> log.info(board));
+    }
 }
